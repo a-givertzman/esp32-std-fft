a613462ae7c0daf233f99bdeff990df305589b00
a-givertzman <a.givertzman@icloud.com>
2023-04-25 12:00:59 +0300

    initial wifi + read adc1_gpio34

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 // use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 // fn main() {
@@ -22,16 +24,22 @@
 // clear && cargo build && espflash --monitor /dev/ttyUSB0 ./target/xtensa-esp32-espidf/debug/esp32-std-fft
 
 use std::{
-    thread::sleep,
-    time::Duration,
+    thread::{
+        self,
+        sleep,
+    },
+    time::Duration, net::{TcpListener, TcpStream, Ipv4Addr},
+    io::{Result, Write, Read}, env,
 };
 use esp_idf_sys as _;
 use esp_idf_hal::{
     peripherals::Peripherals, 
     adc,
-    gpio,
+    gpio::PinDriver, 
+    // spi::Dma, 
+    delay::FreeRtos,
 };
-use esp_idf_sys::{self, *};
+// use esp_idf_sys::{self, *};
 
 use esp_idf_svc::{
     wifi::EspWifi,
@@ -40,18 +48,44 @@ use esp_idf_svc::{
 };
 use embedded_svc::wifi::{ClientConfiguration, Wifi, Configuration};
 
-fn main(){
+use heapless::{
+    spsc::{
+        Queue, 
+    }, 
+    Vec, 
+};
+
+
+const QSIZE: usize = 64;
+const QSIZE_ADD: usize = QSIZE + 1;
+static mut QUEUE: Queue<u16, QSIZE_ADD> = Queue::new();
+
+fn main() {
     esp_idf_sys::link_patches();//Needed for esp32-rs
     println!("Entered Main function!");
     let peripherals = Peripherals::take().unwrap();
+    
+    let mut led = PinDriver::output(peripherals.pins.gpio4).unwrap();
+
+    for _ in 0..4 {
+        led.set_high().unwrap();
+        // we are sleeping here to make sure the watchdog isn't triggered
+        FreeRtos::delay_ms(100);
+
+        led.set_low().unwrap();
+        FreeRtos::delay_ms(100);
+    }
+
     let sys_loop = EspSystemEventLoop::take().unwrap();
     let nvs = EspDefaultNvsPartition::take().unwrap();
     
     // #[cfg(esp32)]
     // let mut a2 = peripherals.pins.gpio34.into_analog_atten_11db()?;
+    let adc_config = adc::config::Config::new()
+        .calibration(true);
     let mut adc1 = adc::AdcDriver::new(
         peripherals.adc1,
-        &adc::config::Config::new().calibration(true),
+        &adc_config,
     ).unwrap();
 
 
@@ -78,14 +112,24 @@ fn main(){
         println!("Waiting for station {:?}", config);
         sleep(Duration::new(1,0));
     }
-    println!("Should be connected now");
+    println!("Wifi should be connected now");
+    let ipInfo = wifi_driver.sta_netif().get_ip_info().unwrap();
+
     println!("IP info: {:?}", wifi_driver.sta_netif().get_ip_info().unwrap());
+    test_tcp_bind(ipInfo.ip, 8080).unwrap();
+    let queue = unsafe {&mut QUEUE};
+    let cycleDelay = Duration::from_secs_f64(0.01);
     loop{
-        sleep(Duration::from_secs_f64(0.5));
+        sleep(cycleDelay);
         match adc1.read(&mut a2) {
             Ok(value) => {
-                println!("ADC1 read ch0 value: {:?}", value);
-
+                // println!("ADC1 read ch0 value: {:?}", value);
+                match queue.enqueue(value) {
+                    Ok(_) => {},
+                    Err(err) => {
+                        println!("add to queue error: {:?}", err);
+                    },
+                };            
             },
             Err(err) => {
                 println!("ADC1 read ch0 error: {:?}", err);
@@ -93,4 +137,90 @@ fn main(){
         }
     }
 
+}
+
+
+fn test_tcp_bind(ip: Ipv4Addr, port: u16) -> Result<()> {
+    fn test_tcp_bind_accept(ip: Ipv4Addr, port: u16) -> Result<()> {
+        println!("About to bind a simple echo service to port 8080");
+
+        let listener = TcpListener::bind("0.0.0.0:8080")?;
+        //     SocketAddr::V4(
+        //         SocketAddrV4::new(
+        //             ip, 
+        //             port,
+        //         )
+        //     )
+        // )?;
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("Accepted client");
+
+                    thread::spawn(move || {
+                        test_tcp_bind_handle_client(stream);
+                    });
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                }
+            }
+        }
+
+        unreachable!()
+    }
+
+    fn test_tcp_bind_handle_client(mut stream: TcpStream) {
+        // read 20 bytes at a time from stream echoing back to stream
+        let queue = unsafe {&mut QUEUE};
+        let cycleDelay = Duration::from_secs_f64(0.1);
+        loop {
+            let mut read = [0; 128];
+            match stream.write_all(&read[0..1]) {
+                Ok(_) => {}
+                Err(err) => {
+                    // connection was closed
+                    println!("Socket stream write error (possible connection was closed): {}", err);
+                    break;
+                }
+            };
+            let qLen = queue.len();
+            if qLen >= QSIZE {
+                // let mut v: Vec<usize, QSIZE> = Vec::new();
+                for _ in 0..qLen {
+                    // v.push(queue.dequeue().unwrap()).unwrap();
+                    let value = queue.dequeue().unwrap();
+                    let bytes = value.to_be_bytes();
+                    match stream.write_all(&bytes) {
+                        Ok(_) => {}
+                        Err(err) => {
+                            // connection was closed
+                            println!("Socket stream write error (possible connection was closed): {}", err);
+                            break;
+                        }        
+                    }
+                }
+            };
+            sleep(cycleDelay);
+            // FreeRtos::delay_ms(100);
+
+            // match stream.read(&mut read) {
+            //     Ok(n) => {
+            //         if n == 0 {
+            //             // connection was closed
+            //             break;
+            //         }
+            //         stream.write_all(&read[0..n]).unwrap();
+            //     }
+            //     Err(err) => {
+            //         panic!("{}", err);
+            //     }
+            // }
+        }
+    }
+
+    thread::spawn(move || test_tcp_bind_accept(ip, port).unwrap());
+
+    Ok(())
 }
